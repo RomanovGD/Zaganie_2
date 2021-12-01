369a4c1a3be84562a29cabf6d3db2090a9ab143a
RomanovGD <95343882+RomanovGD@users.noreply.github.com>
2021-12-01 17:23:56 +0300

    Add files via upload

---
+++ pr2_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pr2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Задание I.\nТочка в области?\n");
+            try
+            {
+                Console.Write("x: ");
+                x = double.Parse(Console.ReadLine());
+                Console.Write("y: ");
+                y = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                if (x * x + y * y < 100 && y < (Math.Abs(x)))
+                    Console.WriteLine("Да");
+                else if (x * x + y * y == 625 || y == 0 || y == (Math.Abs(x)))
+                    Console.WriteLine("На границе");
+                else Console.WriteLine("Нет");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Что-то не так");
+            }
+
+        }
+    }
+}
